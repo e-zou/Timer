d0f93ce0e6bc307a24f9e2c9235db45e394f1bea
Emily Zou <emily@d-172-25-230-219.dhcp.virginia.edu>
2019-05-29 09:32:16 -0400

    timer types

--- timer/src/TimerCircle.js
+++ timer/src/TimerCircle.js
@@ -1,5 +1,6 @@
 import React, { Components } from 'react';
 
+// From Florin Pop: https://www.florin-pop.com/blog/2019/05/countdown-built-with-react/
 // From StackOverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
     var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
