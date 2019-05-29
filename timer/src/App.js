f9d6ed06f4399784405a03e9f89b5e0e2a883d4a
Luke Lukeandrewforeman Andrew Foreman <lukeandrewforeman@d-172-25-250-128.dhcp.virginia.edu>
2019-05-29 15:01:16 -0400

    added material ui

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.css';
 import MainPage from './Components/mainpage.js';
 import Header from './Components/header.js';
+import Button from '@material-ui/core/Button';
+
+//This app uses react-router to create and navigate between multiple pages
+//This tutorial is helpful for understanding this: https://blog.pshrmn.com/simple-react-router-v4-tutorial/
 import { BrowserRouter } from 'react-router-dom';
+
 
 function App() {
   return (
